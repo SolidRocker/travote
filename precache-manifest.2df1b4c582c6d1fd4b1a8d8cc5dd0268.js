7b5d152ac265cc4895cf1ab25901ddcdf1ad7b90
SolidRocker <kamtk.tyler@gmail.com>
2019-07-24 03:14:14 +0800

    Updates

--- precache-manifest.2df1b4c582c6d1fd4b1a8d8cc5dd0268.js
+++ precache-manifest.2df1b4c582c6d1fd4b1a8d8cc5dd0268.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c700e4bd8889e42dca9bc16fd0c1b7e3",
+    "revision": "5ef790d0f26fa6b17e7c542306384463",
     "url": "/travote/index.html"
   },
   {
-    "revision": "ace55f32e153264c6578",
+    "revision": "24a188dc2f071b508c29",
     "url": "/travote/static/css/main.70fa2b04.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/travote/static/js/2.758181b2.chunk.js"
   },
   {
-    "revision": "ace55f32e153264c6578",
-    "url": "/travote/static/js/main.6a1a8473.chunk.js"
+    "revision": "24a188dc2f071b508c29",
+    "url": "/travote/static/js/main.2228fccd.chunk.js"
   },
   {
     "revision": "90210c86a3ab662bbbe4",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/travote/precache-manifest.f2802b9b1f89338b570bec551e339fb6.js"
+  "/travote/precache-manifest.2df1b4c582c6d1fd4b1a8d8cc5dd0268.js"
 );
 
 self.addEventListener('message', (event) => {
